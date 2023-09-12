d96a6adc1adebfbcad8a54df7c19ac0a65e64bba
Theo <theo@noreply.org>
2023-09-12 03:05:43 -0700

    Prepare package to self-host in Gitea npm registry

--- src/mimc.js
+++ src/mimc.js
@@ -1,3 +1,3 @@
-const { mimcsponge } = require('circomlib')
-const { bigInt } = require('snarkjs')
+const { mimcsponge } = require('@tornado/circomlib')
+const { bigInt } = require('@tornado/snarkjs')
 module.exports = (left, right) => mimcsponge.multiHash([bigInt(left), bigInt(right)]).toString()
