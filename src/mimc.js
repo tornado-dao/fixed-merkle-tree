9cf202ce364bee16e115b40f8e2707f4f5a3e352
Theo <theo@noreply.org>
2023-09-18 01:31:37 -0700

    Update dependencies & fix package name to self-hosted registry

--- src/mimc.js
+++ src/mimc.js
@@ -1,3 +1,4 @@
-const { mimcsponge } = require('circomlib')
-const { bigInt } = require('snarkjs')
-module.exports = (left, right) => mimcsponge.multiHash([bigInt(left), bigInt(right)]).toString()
+const { mimcsponge } = require("@tornado/circomlib");
+const { bigInt } = require("@tornado/snarkjs");
+module.exports = (left, right) =>
+  mimcsponge.multiHash([bigInt(left), bigInt(right)]).toString();
